0654f260c43b1fa6fd264337593e70728321b1fd
YajwinGrover <yajwin.grover@gmail.com>
2022-05-06 10:57:01 -0700

    We added de images.

--- main.py
+++ main.py
@@ -3,37 +3,45 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
-window = pygame.display.set_mode((1000,1000*9/16))
-pygame.draw.circle(window,'WHITE',(200,50),30 )
+blackQueen = pygame.image.load('ChessKingSacrafice/blackQueen.png')
+window = pygame.display.set_mode((1280,1280*9/16))
+window.fill('#966F33')
+boxSize = 90
+for row in range(0, 8):
+    for col in range(0, 8):
+        if ((row + col) % 2 == 0):
+            pygame.draw.rect(window, '#382e12', (boxSize*row, boxSize*col, boxSize, boxSize))
+
+pygame.draw.rect(window,'BLACK',(0,0,720,720),2)
 FPS = pygame.time.Clock()
 FPS.tick(60)
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("AvatarMaker copy.png")
-        self.rect = self.image.get_rect()
-        self.rect.center = (160,520)
-
-    def update(self):
-        pressedKeys = pygame.key.get_pressed()
-        if pressedKeys[K_w]:
-            self.rect.move_ip(0,-5)
-        if pressedKeys[K_s]:
-            self.rect.move_ip(0,5)
-    def draw(self,surface):
-        surface.blit(self.image,self.rect)
-
-P1 = Player()
+# class Player(pygame.sprite.Sprite):
+#     def __init__(self):
+#         super().__init__()
+#         self.image = pygame.image.load("AvatarMaker copy.png")
+#         self.rect = self.image.get_rect()
+#         self.rect.center = (160,520)
+#
+#     def update(self):
+#         pressedKeys = pygame.key.get_pressed()
+#         if pressedKeys[K_w]:
+#             self.rect.move_ip(0,-5)
+#         if pressedKeys[K_s]:
+#             self.rect.move_ip(0,5)
+#     def draw(self,surface):
+#         surface.blit(self.image,self.rect)
+#
+# P1 = Player()
 while True:
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    P1.update()
-    window.fill('WHITE')
-    P1.draw(window)
+
+
+    window.blit(blackQueen,(0,0))
     pygame.display.update()
     FPS.tick(60)
 
